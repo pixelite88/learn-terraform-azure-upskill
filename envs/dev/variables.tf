81bbcac278960a6e4327082a7193dcdec83bf8e4
Grzegorz Martowski <grzegorz.martowski@xebia.com>
2025-08-14 13:07:55 +0200

    Remove unused variables.tf

--- envs/dev/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "function_app_name" {}
-variable "location" {}
-variable "resource_group_name" {}
-variable "service_plan_id" {}
-variable "storage_account_name" {}
-variable "storage_account_access_key" {}
-variable "instrumentation_key" {}
